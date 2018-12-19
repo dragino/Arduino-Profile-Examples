d252eb8fb71af6c579eb58f3946a1e1c3756fbbf
edwin <edwin@dragino.com>
2018-12-19 19:40:37 +0800

    add LoRa examples for LG02

---
+++ libraries/Dragino/examples/LoRa/LoRaReceiver/LoRaReceiver.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915600000)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+    LoRa.setSyncWord(0x34);
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
