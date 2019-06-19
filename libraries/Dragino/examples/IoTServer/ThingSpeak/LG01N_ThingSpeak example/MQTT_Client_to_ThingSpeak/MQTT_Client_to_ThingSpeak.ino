656be9cbce75213c3fd01e3c7108faf715b59c4d
edwin <edwin@dragino.com>
2019-06-19 21:53:12 +0800

    Add downlink logic and shorten the name.

--- libraries/Dragino/examples/IoTServer/ThingSpeak/LG01N_ThingSpeak example/MQTT_Client_to_ThingSpeak/MQTT_Client_to_ThingSpeak.ino
+++ libraries/Dragino/examples/IoTServer/ThingSpeak/LG01N_ThingSpeak example/MQTT_Client_to_ThingSpeak/MQTT_Client_to_ThingSpeak.ino
@@ -2,7 +2,8 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-// Singleton instance of the radio driver
+// User manual see Single Channel LoRa IoT Kit v2 User Manual_v1.0.2.pdf from http://www.dragino.com/downloads/index.php?dir=LoRa_IoT_Kit/v2-Kit/
+// Example 2: Test with a MQTT IoT Server
 
 dht DHT;
 #define DHT11_PIN A0
@@ -12,6 +13,7 @@ char tem_1[8]={"\0"},hum_1[8]={"\0"};
 char *node_id = "<5678>";  //From LG01 via web Local Channel settings on MQTT.Please refer <> dataformat in here. 
 uint8_t datasend[36];
 unsigned int count = 1; 
+unsigned long new_time,old_time=0;
 
 void setup()
 {
@@ -23,12 +25,22 @@ void setup()
           Serial.println("Starting LoRa failed!");
           while (1);
       }
+      // Setup Spreading Factor (6 ~ 12)
+      LoRa.setSpreadingFactor(7);
+      
+      // Setup BandWidth, option: 7800,10400,15600,20800,31250,41700,62500,125000,250000,500000
+      //Lower BandWidth for longer distance.
+      LoRa.setSignalBandwidth(125000);
+      
+      // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
+      LoRa.setCodingRate4(5);
       LoRa.setSyncWord(0x34); 
       Serial.println("LoRa init succeeded.");
       
       pinMode(flame_pin,INPUT);
       attachInterrupt(1,fire,FALLING);
-        
+      LoRa.onReceive(onReceive);   
+      LoRa.receive();     
 }
 
 void dhtTem()
@@ -85,21 +97,37 @@ void SendData()
      LoRa.beginPacket();
      LoRa.print((char *)datasend);
      LoRa.endPacket();
-     Serial.println("The packet is send successful");
-     delay(60000);  // one minute
+     Serial.println("Packet Sent");
 }    
     
 
 
 void loop()
 {
-    Serial.print("###########    ");
-    Serial.print("COUNT=");
-    Serial.print(count);
-    Serial.println("    ###########");
-     count++;
-     dhtTem();
-     dhtWrite();
-     SendData();
+    new_time=millis();
+    if (new_time - old_time >= 60000 || old_time == 0)
+    {
+      old_time = new_time;
+      Serial.print("###########    ");
+      Serial.print("COUNT=");
+      Serial.print(count);
+      Serial.println("    ###########");
+      count++;
+      dhtTem();
+      dhtWrite();
+      SendData();
+      LoRa.receive();
+    }
 }
 
+void onReceive(int packetSize) {
+ 
+  // received a packet
+  Serial.print("Received packet : ");
+
+  // read packet
+  for (int i = 0; i < packetSize; i++) {
+      Serial.print((char)LoRa.read());
+  }
+  Serial.print("\n\r");  
+}
